8ffe6b8f866d0d4d925f041af299860eed866cc3
Merajul Islam <merajul_mis@mascoknit.com>
2025-03-05 22:37:36 +0600

    btn count added

--- script.js/task.js
+++ script.js/task.js
@@ -1,8 +1,70 @@
+document.getElementById('btn1').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
 
+    //  pointer-events-none opacity-50
+document.getElementById('btn2').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
 
-// document.getElementById('blog').addEventListener('click',function(){
-//     window.location.href = "./blog.html";
-// })
+document.getElementById('btn3').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
+document.getElementById('btn4').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
+document.getElementById('btn5').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
 
-
-
+document.getElementById('btn6').addEventListener('click', function () {
+        alert('Board update successfully');
+        // Task assign
+        const taskAssigned = document.getElementById('task-assigned').innerText;
+        const taskRemain = parseInt(taskAssigned) - 1;
+        document.getElementById('task-assigned').innerText = taskRemain;
+        // Total Task
+        const taskCount = document.getElementById('task-count').innerText;
+        const totalTask = parseInt(taskCount) + 1;
+        document.getElementById('task-count').innerText = totalTask;
+    })
